35c0ac3c483a940b00234fdfdb0bb1d9228b2833
risent <bsstr@yandex.ru>
2014-09-14 17:19:30 +0400

    remove unnecessary files

--- patterns.php
+++
@@ -1,5 +0,0 @@
-<?php
-$name_pattern = 'pattern="^[A-Za-zА-Яа-я_\s]*[A-Za-zА-Яа-я-]+[A-Za-zА-Яа-я_\s]*$"';
-$name_ru_pattern = 'pattern="^[А-Яа-я_\s]*[А-Яа-я-]+[А-Яа-я_\s]*$"';
-$name_en_pattern = 'pattern="^[A-Za-z_\s]*[A-Za-z-]+[A-Za-z_\s]*$"';
-?>
